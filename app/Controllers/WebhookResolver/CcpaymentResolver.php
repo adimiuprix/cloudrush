20e4468322bedf3fae3a45189949f50b5cad9f9b
adimiuprix <miuprix@gmail.com>
2024-09-12 21:45:19 +0700

    update deposit

--- app/Controllers/WebhookResolver/CcpaymentResolver.php
+++ app/Controllers/WebhookResolver/CcpaymentResolver.php
@@ -3,14 +3,44 @@
 namespace App\Controllers\WebhookResolver;
 
 use App\Controllers\BaseController;
+use App\Models\DepositModel;
+use App\Models\UserPlanHistoryModel;
 
 class CcpaymentResolver extends BaseController
 {
     public function resolver()
     {
-        echo "Halooooooo";
-        // $jsonData = $this->request->getJSON(true);
-        // $filePath = WRITEPATH . 'data.json';
-        // file_put_contents($filePath, json_encode($jsonData));
+        $data = $this->request->getJSON(true);
+        $type = $data['type'];
+        $orderId = $data['msg']['orderId'];
+        $status = $data['msg']['status'];
+
+        $deposit_model = new DepositModel();
+        $user_plan_history_model = new UserPlanHistoryModel();
+
+        if($type == 'ApiDeposit'){
+            switch ($status) {
+                case 'Procesing':
+                    $order = $deposit_model->where('hash_tx', $orderId)->first();
+                    $deposit_model->update($order['id'], ['status' => $status]);
+                  break;
+                case 'Success':
+                    $order = $deposit_model->where('hash_tx', $orderId)->first();
+                    $deposit_model->update($order['id'], ['status' => $status]);
+                  break;
+              }
+        }
     }
 }
+
+
+// {
+//     "type": "ApiDeposit",
+//     "msg": {
+//       "recordId": "20240313121919...",
+//       "orderId": "202403131218361...",
+//       "coinId": 1329,
+//       "coinSymbol": "MATIC",
+//       "status": "Success"
+//     }
+//   }
