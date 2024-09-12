2dbbd4889ee82687de1391f551aef1430c276f94
adimiuprix <miuprix@gmail.com>
2024-09-12 20:14:54 +0700

    fix webhook

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -36,4 +36,4 @@ $routes->group('', function($routes) {
     $routes->post('wd_request', 'WithdrawController::withdrawreq');
 });
 
-$routes->get('ccpayment', 'WebhookResolverController::resolver');
+$routes->get('ccpayment', 'WebhookResolver\CcPaymentWebhookController::resolver');
