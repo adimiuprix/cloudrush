bd6c66d15b7c89c2264fffc75836a2284276a208
adimiuprix <miuprix@gmail.com>
2024-09-12 15:30:42 +0700

    ccpayment

--- app/Controllers/PaymentController.php
+++ app/Controllers/PaymentController.php
@@ -7,6 +7,7 @@ use App\Models\DepositModel;
 use App\Models\PlanModel;
 use App\Models\UserPlanHistoryModel;
 use Takuya\RandomString\RandomString;
+use GuzzleHttp\Client;
 
 class PaymentController extends BaseController
 {
@@ -22,6 +23,7 @@ class PaymentController extends BaseController
         // Get plan id
         $get_plan = $plan_model->where('id', $plan_id)->get()->getRow();
 
+        // Run deposit methods
         switch ($deposit_type) {
             case 'faucetpay':
                 $this->faucetpay();
@@ -43,6 +45,7 @@ class PaymentController extends BaseController
         return view('user/payment', $data);
     }
 
+    // Manual Payments
     public function manual(int $id, int $plan_id, float $amount, string $rand)
     {
         $deposit_model = new DepositModel();
@@ -66,6 +69,65 @@ class PaymentController extends BaseController
     }
 
     public function faucetpay(){
-
+        $params = [
+            'merchant_username' => 'MERCHANT_NAME',
+            'item_description' => 'ITEM_DESCRPTION',
+            'amount1' => 'AMOUNT1',
+            'currency1' => 'USD',
+            'currency2' => 'TRX',
+            'custom' => 'ORDER_ID',
+            'callback_url' => 'CALLBACK_URL',
+            'success_url' => 'SUCCESS_URL',
+            'cancel_url' => 'CANCEL_URL'
+        ];
+        $client = new Client();
+        $response = $client->post('https://faucetpay.io/merchant/webscr', [
+            'form_params' => $params,
+        ]);
     }
+
+    public function ccpayment(){
+        $app_id = "*** your app_id ***";
+        $app_secret = "*** your app_secret ***";
+        $url = "*** your_url ***";
+
+        $content = [
+            "coinId"  => 1280,
+            "price"   => "1",
+            "orderId" => "testorderid111133",
+            "chain"   => "POLYGON"
+        ];
+
+        $timestamp = time();
+        $body = json_encode($content);
+        $sign_text = $app_id . $timestamp;
+
+        if (strlen($body) != 2) {
+            $sign_text .= $body;
+        } else {
+            $body = "";
+        }
+
+        $server_sign = hash_hmac('sha256', $sign_text, $app_secret);
+
+        $client = new Client();
+        try {
+            $response = $client->post($url, [
+                'headers' => [
+                    'Content-Type' => 'application/json;charset=utf-8',
+                    'Appid'        => $app_id,
+                    'Sign'         => $server_sign,
+                    'Timestamp'    => $timestamp,
+                ],
+                'body' => $body,
+            ]);
+
+            $result = json_decode($response->getBody(), true);
+            return $this->response->setJSON($result);
+
+        } catch (\Exception $e) {
+            return $this->response->setJSON(['error' => $e->getMessage()]);
+        }
+    }
+
 }
