64960ba19c51ba0edd66aa1eed76688aff302c00
adimiuprix <miuprix@gmail.com>
2024-11-05 20:36:12 +0700

    panah 1

--- app/Database/Migrations/2024-08-27-020428_Settings.php
+++ app/Database/Migrations/2024-08-27-020428_Settings.php
@@ -10,48 +10,48 @@ class Settings extends Migration
     {
         $this->forge->addField([
             'id' => [
-                'type'           => 'INT',
-                'constraint'     => 100,
-                'unsigned'       => true,
-                'auto_increment' => true,
+                'type'              => 'INT',
+                'constraint'        => 100,
+                'unsigned'          => true,
+                'auto_increment'    => true,
             ],
             'site_name' => [
-                'type'       => 'VARCHAR',
-                'constraint'     => 255,
-                'null'  => false
+                'type'              => 'VARCHAR',
+                'constraint'        => 255,
+                'null'              => false
             ],
             'keywords' => [
-                'type'       => 'VARCHAR',
-                'constraint'     => 255,
-                'null'  => false
+                'type'              => 'VARCHAR',
+                'constraint'        => 255,
+                'null'              => false
             ],
             'description' => [
-                'type'       => 'TEXT',
-                'null'  => false
+                'type'              => 'TEXT',
+                'null'              => false
             ],
             'start_from' => [
-                'type'       => 'DATE',
-                'null'  => false
+                'type'              => 'DATE',
+                'null'              => false
             ],
             'curr_name'     => [
-                'type'       => 'VARCHAR',
-                'constraint'     => 50,
-                'null'  => false
+                'type'              => 'VARCHAR',
+                'constraint'        => 50,
+                'null'              => false
             ],
             'curr_code'     => [
-                'type'       => 'VARCHAR',
-                'constraint'     => 50,
-                'null'  => false
+                'type'              => 'VARCHAR',
+                'constraint'        => 50,
+                'null'              => false
             ],
             'deposit_method' => [
-                'type'       => 'ENUM',
-                'constraint' => ['manual', 'ccpayments'],
-                'default' => 'manual',
+                'type'              => 'ENUM',
+                'constraint'        => ['manual', 'ccpayments'],
+                'default'           => 'manual',
             ],
             'withdraw_method' => [
-                'type'       => 'ENUM',
-                'constraint' => ['manual', 'ccpayments'],
-                'default' => 'manual',
+                'type'              => 'ENUM',
+                'constraint'        => ['manual', 'ccpayments'],
+                'default'           => 'manual',
             ],
             'created_at DATETIME DEFAULT CURRENT_TIMESTAMP',
         ]);
@@ -59,14 +59,14 @@ class Settings extends Migration
         $this->forge->createTable('settings');
         $init_data = [
             [
-                'site_name'     => 'Ferontron',
-                'keywords'      => 'mining trx, mining, app, web mining, crypto earning',
-                'description'     => 'Crypto mining that can give you earning',
-                'start_from'     => date('Y-m-d H:i:s'),
-                'curr_name'     => 'Troncoin',
-                'curr_code'     => 'TRX',
-                'deposit_method' => 'manual',
-                'withdraw_method' => 'manual',
+                'site_name'             => 'Ferontron',
+                'keywords'              => 'mining trx, mining, app, web mining, crypto earning',
+                'description'           => 'Crypto mining that can give you earning',
+                'start_from'            => date('Y-m-d H:i:s'),
+                'curr_name'             => 'Troncoin',
+                'curr_code'             => 'TRX',
+                'deposit_method'        => 'manual',
+                'withdraw_method'       => 'manual',
             ],
         ];
         $this->db->table('settings')->insertBatch($init_data);
